1e3a182da53f62f55a67e441c3ab9b7f560ca293
Brad Chao <brad@brad.tw>
2016-10-04 14:07:52 +0800

    v8

--- app/src/main/java/brad/tw/myupload2/CameraPreview.java
+++ app/src/main/java/brad/tw/myupload2/CameraPreview.java
@@ -5,18 +5,37 @@ import android.hardware.Camera;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.io.IOException;
+
 /**
  * Created by user on 2016/10/4.
  */
 
 public class CameraPreview extends SurfaceView implements  SurfaceHolder.Callback{
+    private Camera mCamera;
+    private SurfaceHolder holder;
+
+
     public CameraPreview(Context context, Camera camera) {
         super(context);
+
+        mCamera = camera;
+
+        holder = getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
